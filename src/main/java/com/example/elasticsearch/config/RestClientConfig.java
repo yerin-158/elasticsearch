af8d7f4fffaa105817437743d00307df8edf6058
yerin-158 <hanblueblue@gmail.com>
2020-07-20 23:06:41 +0900

    local의 elasticsearch store와 연동하는 설정추가

---
+++ src/main/java/com/example/elasticsearch/config/RestClientConfig.java
@@ -0,0 +1,24 @@
+package com.example.elasticsearch.config;
+
+import ch.qos.logback.core.net.server.Client;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.settings.Settings;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.RestClients;
+import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
+import org.springframework.data.elasticsearch.config.ElasticsearchConfigurationSupport;
+
+@Configuration
+public class RestClientConfig extends AbstractElasticsearchConfiguration {
+
+    @Override
+    @Bean
+    public RestHighLevelClient elasticsearchClient() {
+        final ClientConfiguration clientConfiguration = ClientConfiguration.builder()
+                .connectedTo("localhost:9200")
+                .build();
+        return RestClients.create(clientConfiguration).rest();
+    }
+}
